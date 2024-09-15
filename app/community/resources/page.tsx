40eeeb31117b1738aed8bc9d2e0e2cf3ae84c7ee
lisaorg <amol.yadav.778@gmail.com>
2024-09-15 20:24:26 +0530

    Create page.tsx

---
+++ app/community/resources/page.tsx
@@ -0,0 +1,13 @@
+import CommunityLeftbar from '@/components/CommunityLeftbar';
+
+const ResourcesPage = () => (
+  <div className="flex">
+    <CommunityLeftbar />
+    <div className="flex-1 p-6">
+      <h1 className="text-2xl">Resources</h1>
+      {/* Add Resources content here */}
+    </div>
+  </div>
+);
+
+export default ResourcesPage;
