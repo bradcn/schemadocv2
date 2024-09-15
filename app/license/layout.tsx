9479094c3d99b2772b0303f22493754079065024
lisaorg <amol.yadav.778@gmail.com>
2024-09-15 09:21:20 +0530

    Update layout.tsx

--- app/license/layout.tsx
+++ app/license/layout.tsx
@@ -1,6 +1,6 @@
 import { PropsWithChildren } from "react";
 
-export default function BlogLayout({ children }: PropsWithChildren) {
+export default function LicenseLayout({ children }: PropsWithChildren) {
   return (
     <div className="flex flex-col items-start justify-center pt-8 pb-10 md:w-[70%] mx-auto">
       {children}
