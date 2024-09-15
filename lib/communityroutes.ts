608983db355a3d66568c45f71d265e821fa263d0
lisaorg <amol.yadav.778@gmail.com>
2024-09-15 20:22:22 +0530

    Create communityroutes.ts

---
+++ lib/communityroutes.ts
@@ -0,0 +1,5 @@
+export const communityroutes = [
+  { name: 'General Discussions', href: '/community/general' },
+  { name: 'Events', href: '/community/events' },
+  { name: 'Resources', href: '/community/resources' },
+];
