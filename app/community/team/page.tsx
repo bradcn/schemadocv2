1a62b73b753a56d88bfdb08ca1d777d174c31a30
lisaorg <amol.yadav.778@gmail.com>
2024-09-21 21:03:30 +0530

    Update page.tsx

--- app/community/team/page.tsx
+++ app/community/team/page.tsx
@@ -1,5 +1,5 @@
-import Link from "next/link"
-import ProfileCard from "@/components/profilecard"
+import Link from "next/link";
+import ProfileCard from "@/components/profilecard";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -9,33 +9,37 @@ export const metadata: Metadata = {
 };
 
 export default function TeamPage() {
-  return(
+  return (
     <main className="max-w-4xl mx-auto my-6">
       <h1 className="text-4xl font-bold my-4">Team</h1>
       <p className="text-lg my-4">
         The Schemadoc team works on the core functionality, as well as the Schemadoc documentation website.
       </p>
       <br />
-      <p className="text-lg my-4">
-        Current members of the Schemadoc team are listed below.
-      </p>
-      <ProfileCard
-  name="Amol Yadav"
-  title="Admin"
-  imgUrl="https://amyssnippet.vercel.app/PSX_20240219_195257.jpg"
-  githubUrl="https://github.com/amyssnippet"
-  portfolioUrl="https://amyssnippet.vercel.app/"
-  devtoUrl="https://dev.to/amyssnippet"
-/>
-     <br />
-      <ProfileCard
-  name="Dinesh Verma"
-  title="Contributor"
-  imgUrl="https://dineshverma.vercel.app/dinesh1.jpg"
-  githubUrl="https://github.com/DineshVerma-dev"
-  portfolioUrl="https://dineshverma.vercel.app/"
-  devtoUrl="https://dev.to/"
-/>
+      <p className="text-lg my-4">Current members of the Schemadoc team are listed below.</p>
+      <div className="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2">
+        <div className="flex justify-center">
+          <ProfileCard
+            name="Amol Yadav"
+            title="Admin"
+            imgUrl="https://amyssnippet.vercel.app/PSX_20240219_195257.jpg"
+            githubUrl="https://github.com/amyssnippet"
+            portfolioUrl="https://amyssnippet.vercel.app/"
+            devtoUrl="https://dev.to/amyssnippet"
+          />
+        </div>
+
+        <div className="flex justify-center">
+          <ProfileCard
+            name="Dinesh Verma"
+            title="Contributor"
+            imgUrl="https://dineshverma.vercel.app/dinesh1.jpg"
+            githubUrl="https://github.com/DineshVerma-dev"
+            portfolioUrl="https://dineshverma.vercel.app/"
+            devtoUrl="https://dev.to/"
+          />
+        </div>
+      </div>
     </main>
   );
 }
