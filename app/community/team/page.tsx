a5084b1e505269aa1b7f6e99aa2989531852f2f5
lisaorg <amol.yadav.778@gmail.com>
2024-09-21 17:01:53 +0530

    Update page.tsx

--- app/community/team/page.tsx
+++ app/community/team/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link"
 import ProfileCard from "@/components/profilecard"
+import PSX_20240219_195257 from "@/public/PSX_20240219_195257.jpg"
 
 export default function TeamPage() {
   return(
@@ -7,7 +8,7 @@ export default function TeamPage() {
       <ProfileCard
   name="Amol Yadav"
   title="Admin"
-  imgUrl="https://amyssnippet.vercel.app/PSX_20240219_195257.jpg"
+  imgUrl="PSX_20240219_195257"
   githubUrl="https://github.com/amyssnippet"
   portfolioUrl="https://amyssnippet.vercel.app/"
   devtoUrl="https://dev.to/amyssnippet"
