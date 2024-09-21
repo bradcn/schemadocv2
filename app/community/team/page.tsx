694c23d180e315333110a4ba2a61013942b77d02
lisaorg <amol.yadav.778@gmail.com>
2024-09-21 16:23:30 +0530

    Update page.tsx

--- app/community/team/page.tsx
+++ app/community/team/page.tsx
@@ -2,8 +2,14 @@ import Link from "next/link";
 
 export default function TeamPage() {
   return(
-    <>
-      this is team page
-    </>
+    <main>
+      <ProfileCard
+  name="Amol Yadav"
+  title="Admin"
+  githubUrl="https://github.com/amyssnippet"
+  portfolioUrl="https://amyssnippet.vercel.app/"
+  devtoUrl="https://dev.to/amyssnippet"
+/> 
+    </main>
   );
 }
