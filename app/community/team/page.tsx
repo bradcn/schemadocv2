12277b109824c0151a0bca5bc9e34d73bc5ea3c3
lisaorg <amol.yadav.778@gmail.com>
2024-09-21 21:10:33 +0530

    Update page.tsx

--- app/community/team/page.tsx
+++ app/community/team/page.tsx
@@ -19,25 +19,29 @@ export default function TeamPage() {
       <p className="text-lg my-4">Current members of the Schemadoc team are listed below.</p>
       <div className="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2">
         <div className="flex justify-center">
-          <ProfileCard
-            name="Amol Yadav"
-            title="Admin"
-            imgUrl="https://amyssnippet.vercel.app/PSX_20240219_195257.jpg"
-            githubUrl="https://github.com/amyssnippet"
-            portfolioUrl="https://amyssnippet.vercel.app/"
-            devtoUrl="https://dev.to/amyssnippet"
-          />
+          <div className="w-full max-w-xs">
+            <ProfileCard
+              name="Amol Yadav"
+              title="Admin"
+              imgUrl="https://amyssnippet.vercel.app/PSX_20240219_195257.jpg"
+              githubUrl="https://github.com/amyssnippet"
+              portfolioUrl="https://amyssnippet.vercel.app/"
+              devtoUrl="https://dev.to/amyssnippet"
+            />
+          </div>
         </div>
 
         <div className="flex justify-center">
-          <ProfileCard
-            name="Dinesh Verma"
-            title="Contributor"
-            imgUrl="https://dineshverma.vercel.app/dinesh1.jpg"
-            githubUrl="https://github.com/DineshVerma-dev"
-            portfolioUrl="https://dineshverma.vercel.app/"
-            devtoUrl="https://dev.to/"
-          />
+          <div className="w-full max-w-xs">
+            <ProfileCard
+              name="Dinesh Verma"
+              title="Contributor"
+              imgUrl="https://dineshverma.vercel.app/dinesh1.jpg"
+              githubUrl="https://github.com/DineshVerma-dev"
+              portfolioUrl="https://dineshverma.vercel.app/"
+              devtoUrl="https://dev.to/"
+            />
+          </div>
         </div>
       </div>
     </main>
