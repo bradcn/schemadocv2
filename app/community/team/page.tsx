f32d783bdb5fff4e526c007e35728da3941fab39
lisaorg <amol.yadav.778@gmail.com>
2024-09-22 15:44:29 +0530

    Update page.tsx

--- app/community/team/page.tsx
+++ app/community/team/page.tsx
@@ -16,30 +16,11 @@ export default function TeamPage() {
         The Schemadoc team works on the core functionality, as well as the Schemadoc documentation website.
       </p>
       <br />
-
-      <p className="text-lg my-4">
-        Current members of the Schemadoc team are listed below.
-      </p>
-      <ProfileCard
-        name="Amol Yadav"
-        title="Admin"
-        imgUrl="https://amyssnippet.vercel.app/PSX_20240219_195257.jpg"
-        githubUrl="https://github.com/amyssnippet"
-        portfolioUrl="https://amyssnippet.vercel.app/"
-        devtoUrl="https://dev.to/amyssnippet"
-      />
-      <br />
-      <ProfileCard
-        name="Dinesh Verma"
-        title="Contributor"
-        imgUrl="https://dineshverma.vercel.app/dinesh1.jpg"
-        githubUrl="https://github.com/DineshVerma-dev"
-        portfolioUrl="https://dineshverma.vercel.app/"
-        devtoUrl="https://dev.to/dinesh_verma"
-      />
-=======
       <p className="text-lg my-4">Current members of the Schemadoc team are listed below.</p>
+      
+      {/* Responsive Wrapper for Profile Cards */}
       <div className="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2">
+        {/* Profile Cards */}
         <div className="flex justify-center">
           <div className="w-full max-w-xs">
             <ProfileCard
@@ -60,13 +41,12 @@ export default function TeamPage() {
               title="Contributor"
               imgUrl="https://dineshverma.vercel.app/dinesh1.jpeg"
               githubUrl="https://github.com/DineshVerma-dev"
-              portfolioUrl="https://dineshverma.vercel.app/"
-              devtoUrl="https://dev.to/"
+              portfolioUrl="https://dineshverma.vercel.app /"
+              devtoUrl="https://dev.to/dinesh_verma"
             />
           </div>
         </div>
       </div>
-
     </main>
   );
 }
